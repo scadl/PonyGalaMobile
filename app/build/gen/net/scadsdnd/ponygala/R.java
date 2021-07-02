3b588d802ed7fe7b4df297510b4d30516950aaf1
Alexander KD <scad.luncher@gmail.com>
2021-07-02 17:59:05 +0300

    res update

--- app/build/gen/net/scadsdnd/ponygala/R.java
+++ app/build/gen/net/scadsdnd/ponygala/R.java
@@ -18,7 +18,8 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040001;
+        public static final int subtitle=0x7f040002;
+        public static final int title=0x7f040001;
     }
     public static final class style {
         public static final int AppTheme=0x7f050000;

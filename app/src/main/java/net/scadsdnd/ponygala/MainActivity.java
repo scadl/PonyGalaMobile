bc047e86f37b8b84931bd7c96b34f4323ab545a5
Alexnadr <scad.luncher@gmail.com>
2021-07-15 18:23:48 +0300

    Basic exit mechanics

--- app/src/main/java/net/scadsdnd/ponygala/MainActivity.java
+++ app/src/main/java/net/scadsdnd/ponygala/MainActivity.java
@@ -2,13 +2,38 @@ package net.scadsdnd.ponygala;
 
 import android.app.*;
 import android.os.*;
+import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends Activity 
 {
+
+    Integer lvl = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+
+        lvl-=1;
+        switch (lvl){
+            case -1:
+                Toast.makeText(MainActivity.this, "Нажмите ещё раз 'Назад', чтобы выйти.", Toast.LENGTH_LONG).show();
+                break;
+            case -2:
+                finish();
+                System.exit(0);
+                break;
+            default:
+                Log.i("lvl", Integer.toString(lvl));
+                break;
+        }
+
     }
 }

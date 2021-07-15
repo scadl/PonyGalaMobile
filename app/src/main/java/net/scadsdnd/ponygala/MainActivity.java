d14269f6c377517784b21e1fd936ed3445a8f901
Alexandr <scad.luncher@gmail.com>
2021-07-15 18:51:29 +0300

    Simple variables and event handlers

--- app/src/main/java/net/scadsdnd/ponygala/MainActivity.java
+++ app/src/main/java/net/scadsdnd/ponygala/MainActivity.java
@@ -1,25 +1,46 @@
 package net.scadsdnd.ponygala;
 
 import android.app.*;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.*;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity 
 {
 
     Integer lvl = 0;
+    Boolean isAdmin = false;
+    ListView catList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        StrictMode.ThreadPolicy mypolicy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(mypolicy);
+
+        SharedPreferences shPrf = MainActivity.this.getPreferences(Context.MODE_PRIVATE);
+        isAdmin = shPrf.getBoolean("admin_mode", false);
+
+        catList = (ListView) findViewById(R.id.catListView);
+        catList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.e("UI", "Cat List click");
+            }
+        });
     }
 
     @Override
     public void onBackPressed() {
-        super.onBackPressed();
+        //super.onBackPressed();
 
         lvl-=1;
         switch (lvl){
@@ -31,7 +52,7 @@ public class MainActivity extends Activity
                 System.exit(0);
                 break;
             default:
-                Log.i("lvl", Integer.toString(lvl));
+                Log.i("SYS", "Level: " + Integer.toString(lvl));
                 break;
         }
 
